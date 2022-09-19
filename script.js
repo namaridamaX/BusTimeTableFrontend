edbcde88cf0905d69f7b741f402a3227b5832b5a
namaridamaX <b2200170@photon.chitose.ac.jp>
2022-09-19 14:42:36 +0900

    現在時刻から一番近いバスの時間を表示させることが可能になった。

--- script.js
+++ script.js
@@ -8,72 +8,97 @@ $(document).ready(function () {
   });
 });
 
+function TimeComparison(time_obj) {
+  let time_array = [];
+  let today = new Date();
+  let value_today = new Date();
+
+  for (i = 0; i < Object.keys(time_obj).length; i++) {
+    s_time = time_obj[i];
+    if (s_time != null) {
+      let hour_mins = s_time.split(":");
+      let hour = hour_mins[0];
+      let mins = hour_mins[1];
+
+      value_today.setHours(Number(hour));
+      value_today.setMinutes(Number(mins));
+
+      if (value_today >= today) {
+        time_array.push(i);
+        if ((time_array.length = 3)) {
+          return time_array;
+        }
+      }
+    }
+  }
+}
+
 function js_check() {
   var StartValue = $("#Start").val();
   var GoalValue = $("#Goal").val();
   switch (StartValue) {
-    case '0':
+    case "0":
       switch (GoalValue) {
-        case '1':
-          var Sta = '千歳駅発';
+        case "1":
+          var Sta = "千歳駅発";
           var Goa = "南千歳駅発";
           break;
-        case '2':
+        case "2":
           var Sta = "千歳駅発";
           var Goa = "研究実験棟発";
           break;
-        case '3':
+        case "3":
           var Sta = "千歳駅発";
           var Goa = "本部棟着";
           break;
       }
       break;
 
-    case '1':
+    case "1":
       switch (GoalValue) {
-        case '2':
+        case "2":
           var Sta = "南千歳駅発";
           var Goa = "研究実験棟発";
           break;
-        case '3':
+        case "3":
           var Sta = "南千歳駅発";
           var Goa = "本部棟着";
           break;
-        case '0':
+        case "0":
           var Sta = "南千歳駅着";
           var Goa = "千歳駅着";
           break;
       }
       break;
 
-    case '2':
+    case "2":
       switch (GoalValue) {
-        case '3':
+        case "3":
           var Sta = "研究実験棟発";
           var Goa = "本部棟着";
           break;
-        case '1':
+        case "1":
           var Sta = "研究実験棟着";
           var Goa = "南千歳駅着";
           break;
-        case '0':
+        case "0":
           var Sta = "研究実験棟着";
           var Goa = "千歳駅着";
           break;
       }
       break;
 
-    case '3':
+    case "3":
       switch (GoalValue) {
-        case '0':
+        case "0":
           var Sta = "本部棟発";
           var Goa = "千歳駅着";
           break;
-        case '1':
+        case "1":
           var Sta = "本部棟発";
           var Goa = "南千歳駅着";
           break;
-        case '2':
+        case "2":
           var Sta = "本部棟発";
           var Goa = "研究実験棟着";
           break;
@@ -90,8 +115,9 @@ function js_check() {
     var str = JSON.parse(json_data);
     console.log(str);
     console.log(str["千歳駅発"][0]);
-
-    document.getElementById("msg1").innerHTML = str[Sta][0];
-    document.getElementById("msg3").innerHTML = str[Goa][0];
+    array = TimeComparison(str[Sta]);
+    i = array[0];
+    document.getElementById("msg1").innerHTML = str[Sta][i];
+    document.getElementById("msg3").innerHTML = str[Goa][i];
   });
 }

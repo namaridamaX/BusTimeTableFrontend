97500a866f83bc57a143b9609d072e7688652dbf
namaridamaX <b2200170@photon.chitose.ac.jp>
2022-09-20 23:34:51 +0900

    エラー処理

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ $(document).ready(function () {
   $("#result2").hide();
   $("#result3").hide();
 
-  $(".SearchButton").click(function () {
-    $("#result1").show();
-    $("#result2").show();
-    $("#result3").show();
-  });
+  // $(".SearchButton").click(function () {
+  //   $("#result1").show();
+  //   $("#result2").show();
+  //   $("#result3").show();
+  // });
 });
 
 $(document).ready(function () {
   const today = new Date();
   var element = document.querySelector('input[type="time"]');
-  var hour = today.getHours();
-  var minute = today.getMinutes();
+  var hour = ("0"+today.getHours()).slice(-2);
+  var minute = ("0"+today.getMinutes()).slice(-2);
   var today_time = hour + ":" + minute;
   element.value = today_time;
 });
@@ -44,11 +44,71 @@ function TimeComparison(time_obj) {
 
       if (value_today >= value_con) {
         time_array.push(i);
-        if ((time_array.length = 3)) {
-          return time_array;
-        }
+        return time_array;
       }
     }
+  }
+  time_array.push(100);
+  return time_array;
+}
+
+function start_disable() {
+  var Goal_value = $("#Goal").val();
+  switch (Goal_value) {
+    case "0":
+      document.getElementById("Start").options[0].disabled = true;
+      document.getElementById("Start").options[1].disabled = false;
+      document.getElementById("Start").options[2].disabled = false;
+      document.getElementById("Start").options[3].disabled = false;
+      break;
+    case "1":
+      document.getElementById("Start").options[0].disabled = false;
+      document.getElementById("Start").options[1].disabled = true;
+      document.getElementById("Start").options[2].disabled = false;
+      document.getElementById("Start").options[3].disabled = false;
+      break;
+    case "2":
+      document.getElementById("Start").options[0].disabled = false;
+      document.getElementById("Start").options[1].disabled = false;
+      document.getElementById("Start").options[2].disabled = true;
+      document.getElementById("Start").options[3].disabled = false;
+      break;
+    case "3":
+      document.getElementById("Start").options[0].disabled = false;
+      document.getElementById("Start").options[1].disabled = false;
+      document.getElementById("Start").options[2].disabled = false;
+      document.getElementById("Start").options[3].disabled = true;
+      break;
+  }
+}
+
+function goal_disable() {
+  var Start_value = $("#Start").val();
+  switch (Start_value) {
+    case "0":
+      document.getElementById("Goal").options[0].disabled = true;
+      document.getElementById("Goal").options[1].disabled = false;
+      document.getElementById("Goal").options[2].disabled = false;
+      document.getElementById("Goal").options[3].disabled = false;
+      break;
+    case "1":
+      document.getElementById("Goal").options[0].disabled = false;
+      document.getElementById("Goal").options[1].disabled = true;
+      document.getElementById("Goal").options[2].disabled = false;
+      document.getElementById("Goal").options[3].disabled = false;
+      break;
+    case "2":
+      document.getElementById("Goal").options[0].disabled = false;
+      document.getElementById("Goal").options[1].disabled = false;
+      document.getElementById("Goal").options[2].disabled = true;
+      document.getElementById("Goal").options[3].disabled = false;
+      break;
+    case "3":
+      document.getElementById("Goal").options[0].disabled = false;
+      document.getElementById("Goal").options[1].disabled = false;
+      document.getElementById("Goal").options[2].disabled = false;
+      document.getElementById("Goal").options[3].disabled = true;
+      break;
   }
 }
 
@@ -127,16 +187,30 @@ function js_check() {
 
   $.get("http://127.0.0.1:5000", function (json_data) {
     var str = JSON.parse(json_data);
-    console.log(str);
     array = TimeComparison(str[Sta]);
     i1 = array[0];
     i2 = i1 + 1;
     i3 = i2 + 1;
-    document.getElementById("msg1").innerHTML = str[Sta][i1];
-    document.getElementById("msg3").innerHTML = str[Goa][i1];
-    document.getElementById("msg4").innerHTML = str[Sta][i2];
-    document.getElementById("msg5").innerHTML = str[Sta][i2];
-    document.getElementById("msg6").innerHTML = str[Sta][i3];
-    document.getElementById("msg7").innerHTML = str[Sta][i3];
+    if (str[Sta][i1] != null) {
+      $("#result1").show();
+      document.getElementById("msg1").innerHTML = str[Sta][i1];
+      document.getElementById("msg3").innerHTML = str[Goa][i1];
+    } else if (str[Sta][i1] == null) {
+      $("#result1").hide();
+    }
+    if (str[Sta][i2] != null) {
+      $("#result2").show();
+      document.getElementById("msg4").innerHTML = str[Sta][i2];
+      document.getElementById("msg5").innerHTML = str[Goa][i2];
+    } else if (str[Sta][i2] == null) {
+      $("#result2").hide();
+    }
+    if (str[Sta][i3] != null) {
+      $("#result3").show();
+      document.getElementById("msg6").innerHTML = str[Sta][i3];
+      document.getElementById("msg7").innerHTML = str[Goa][i3];
+    } else if (str[Sta][i3] == null) {
+      $("#result3").hide();
+    }
   });
 }
